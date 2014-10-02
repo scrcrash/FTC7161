171c658ac52f44b78755e4fd6452892909afec14
Kiduke <jkiduke@yahoo.com>
2014-10-02 07:59:24 -0500

    Moving Files to GItHub
    moved all files we made this year so far into github repository

---
+++ Autonomous Chooser v1.c
@@ -0,0 +1,12 @@
+#include "NXTFileIODefs.h"
+
+int IntArray[i++] = {"RampAutonomous v1.1.c" , "parkingzoneAutonomous v1.c" , "autoDefense v1"}
+
+task main()
+{
+	if(nNxtButtonPressed == 1)
+
+
+
+	nNxtExitClicks = 2; //exit program after 2 clicks
+}
